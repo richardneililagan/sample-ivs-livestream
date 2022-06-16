b4763d2f8d33118597f07ccbe7db5e732239b935
Richard Neil Ilagan <ilagan@amazon.com>
2022-06-16 13:21:47 +0800

    chore: adds shared eslint and prettier configuration

---
+++ config/eslint-config/src/index.js
@@ -0,0 +1,44 @@
+const header = `/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ */`
+
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    jsx: true,
+  },
+
+  env: {
+    jest: true,
+  },
+
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  plugins: ['header'],
+
+  rules: {
+    'header/header': [2, 'block', header],
+
+    '@typescript-eslint/no-empty-interface': 'off',
+    'unicorn/no-null': 'off',
+
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          env: false,
+          props: false,
+          params: false,
+        },
+      },
+    ],
+  },
+}
+
+module.exports = config
---
+++ config/prettier-config/src/index.js
@@ -0,0 +1,11 @@
+const config = {
+  tabWidth: 2,
+  printWidth: 100,
+
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingCommas: 'all',
+}
+
+module.exports = config
